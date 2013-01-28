7d51228fc0850b9c596e64e1aec9f2ec0db4fba0
David Muto <david.muto@gmail.com>
2013-01-28 16:46:16 -0500

    Initial Commit with XCode Project

---
+++ CFormsFlattener/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CFormsFlattener
+//
+//  Created by David Muto on 2013-01-28.
+//  Copyright (c) 2013 David Muto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
